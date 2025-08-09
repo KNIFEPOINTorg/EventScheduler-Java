788a4eec62d0a84e13dbb29354d746676e9d2632
alexamikealson <alexamikealson4444@gmail.com>
2025-08-08 20:35:57 -0700

    Initial commit: Java Event Scheduler application

---
+++ AppController.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.util.List;
+
+public class AppController {
+    private MainFrame mainFrame;
+    private EventListPanel eventListPanel;
+    private UserPanel userPanel;
+    private EventService eventService;
+
+    public AppController(MainFrame mainFrame, EventListPanel eventListPanel, UserPanel userPanel, EventService eventService) {
+        this.mainFrame = mainFrame;
+        this.eventListPanel = eventListPanel;
+        this.userPanel = userPanel;
+        this.eventService = eventService;
+        initListeners();
+    }
+
+    private void initListeners() {
+        JTable table = getEventTable();
+        if (table != null) {
+            table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+                public void valueChanged(ListSelectionEvent e) {
+                    if (!e.getValueIsAdjusting()) {
+                        int selectedRow = table.getSelectedRow();
+                        if (selectedRow >= 0) {
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private JTable getEventTable() {
+        return null;
+    }
+
+    private void openEventForm() {
+        EventFormDialog dialog = new EventFormDialog(mainFrame);
+        dialog.setVisible(true);
+    }
+
+    private void addUser() {
+        JOptionPane.showMessageDialog(mainFrame, "Add User logic here");
+    }
+
+    public void refreshEvents(List<Event> events) {
+        eventListPanel.setEvents(events);
+    }
+
+
+    public void refreshUsers(List<User> users) {
+        userPanel.setUsers(users);
+    }
+} 
---
+++ CLILauncher.java
@@ -0,0 +1,10 @@
+
+
+
+
+public class CLILauncher {
+    public static void main(String[] args) {
+        CLIMenu menu = new CLIMenu();
+        menu.displayMenu();
+    }
+} 
